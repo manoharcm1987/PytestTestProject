0df81ed0dd83a994c5cfa0949823007b59a4be4a
manoharcm <manoharcm1987@gmail.com>
2020-07-19 19:47:28 +0530

    Initial commit for pytest project for azure devops

---
+++ conftest.py
@@ -0,0 +1,65 @@
+"""
+This module contains shared fixtures for web UI tests.
+"""
+from selenium.webdriver import Chrome, Firefox
+from selenium.webdriver.chrome.options import Options
+import pytest
+import json
+
+CONFIG_PATH = '/utilities/config.json'
+DEFAULT_WAIT_TIME = 10
+SUPPORTED_BROWSERS = ['chrome', 'firefox']
+
+
+@pytest.fixture(scope='session')
+def config():
+    # Read the JSON config file and returns it as a parsed dict
+    with open(CONFIG_PATH) as config_file:
+        data = json.load(config_file)
+        return data
+
+
+@pytest.fixture(scope='session')
+def config_browser(config):
+
+    if 'browser' not in config:
+        raise Exception('The config file doesnot contain "browser" tag')
+    elif config['browser'] not in SUPPORTED_BROWSERS:
+        raise Exception(f'{config["browser"]} is not supported browser')
+    else:
+        return config['browser']
+
+
+@pytest.fixture(scope='session')
+def config_wait_time(config):
+    return config['implicit_wait'] if 'implicit_wait' in config else DEFAULT_WAIT_TIME
+
+
+@pytest.fixture(scope='session')
+def get_browser(config_browser, config_wait_time):
+    # Initialize WebDriver
+    chrome_options = Options()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument("--test-type")
+    chrome_options.add_experimental_option("useAutomationExtension", False);
+    # chrome_options.add_argument("--disable-dev-shm-usage")
+    # chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_experimental_option("excludeSwitches", ['enable-automation']);
+    if config_browser == 'chrome':
+        driver = Chrome(options=chrome_options,
+            executable_path='C:/Old D drive/SeleniumWorkSpace/TestProject/drivers/chromedriver.exe')
+    elif config_browser == 'Firefox':
+        raise Exception(f'{config_browser} is not supported for now')
+    else:
+        raise Exception(f'{config_browser} is not supported!!!')
+
+    # Wait implicitly for elements to be ready before attempting interactions
+    driver.implicitly_wait(config_wait_time)
+    driver.maximize_window()
+
+    # Return the driver object at the end of setup
+    yield driver
+
+    # For cleanup, quit the driver
+    driver.quit()
---
+++ tests/test_sample.py
@@ -0,0 +1,43 @@
+from webbrowser import Chrome
+
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import Chrome
+from selenium import webdriver as driver
+import pytest
+import time
+"""
+This module contains DuckDuckGoSearchPage,
+the page object for the DuckDuckGo search page.
+"""
+
+
+class TestSample():
+    SEARCH_INPUT = (By.NAME, 'q')
+
+    def test_search(self):
+        chrome_options = Options()
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument('--ignore-certificate-errors')
+        chrome_options.add_argument("--test-type")
+        chrome_options.add_experimental_option("useAutomationExtension", False);
+        # chrome_options.add_argument("--disable-dev-shm-usage")
+        # chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_experimental_option("excludeSwitches", ['enable-automation']);
+        self.driver = Chrome(options=chrome_options, executable_path='C:/Old D drive/SeleniumWorkSpace/TestProject/drivers/chromedriver.exe')
+        # Wait implicitly for elements to be ready before attempting interactions
+        self.driver.implicitly_wait(5)
+        self.driver.maximize_window()
+        self.driver.get('https://www.google.com')
+        g_search = self.driver.find_element(*self.SEARCH_INPUT)
+        g_search.send_keys('github')
+        g_search.send_keys(Keys.ENTER)
+        time.sleep(5)
+        title = self.driver.title
+        assert "github" in title
+        self.driver.close()
+
+
+
+
