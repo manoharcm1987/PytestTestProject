70c6cface672ab0282d3e1611aee6f709f6b8412
manoharcm <manoharcm1987@gmail.com>
2020-07-19 21:03:17 +0530

    Update chrome executable path

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -21,7 +21,6 @@ class TestSample():
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument('--ignore-certificate-errors')
         chrome_options.add_argument("--test-type")
-        chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_experimental_option("useAutomationExtension", False);
